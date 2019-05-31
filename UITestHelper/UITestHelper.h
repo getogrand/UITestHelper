59bffed9138c69df665ab3ba5e051c5d0b8e542a
Wonyoung Ju <getogrand@gmail.com>
2019-05-31 11:50:17 +0900

    Support carthage

---
+++ UITestHelper/UITestHelper.h
@@ -0,0 +1,19 @@
+//
+//  UITestHelper.h
+//  UITestHelper
+//
+//  Created by WonyoungJu on 31/05/2019.
+//  Copyright © 2019 evict. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UITestHelper.
+FOUNDATION_EXPORT double UITestHelperVersionNumber;
+
+//! Project version string for UITestHelper.
+FOUNDATION_EXPORT const unsigned char UITestHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UITestHelper/PublicHeader.h>
+
+
